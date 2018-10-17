9a48f4acd491de12fdb46a39fae53567b137f31e
kobifrobby <jakob.bran@gmail.com>
2018-10-17 11:40:45 +0200

    fy2

--- app/src/main/java/com/project/jb/newyum/MainActivity.java
+++ app/src/main/java/com/project/jb/newyum/MainActivity.java
@@ -1,15 +1,20 @@
 package com.project.jb.newyum;
 
+import android.Manifest;
 import android.app.DownloadManager;
+import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Gravity;
@@ -18,6 +23,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -32,19 +38,30 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.w3c.dom.Text;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.app.DownloadManager.COLUMN_STATUS;
+import static android.app.DownloadManager.STATUS_RUNNING;
 
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private static final String[] PERMISSIONS_STORAGE = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    private static final String[] PERMISSIONS_INTERNET = {Manifest.permission.INTERNET};
 
     List<String> links;
     List<String> titles1;
 
     private DownloadManager downloadManager;
 
-    android.widget.SearchView searchView;
     private long Music_DownloadId;
 
 
@@ -55,12 +72,16 @@ public class MainActivity extends AppCompatActivity {
         links = new ArrayList<>();
         titles1 = new ArrayList<>();
 
+        verifyPermissions();
+
         final Button convert = (Button) findViewById(R.id.convertbutton);
 
 
         //set filter to only when download is complete and register broadcast receiver
         IntentFilter filter = new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE);
         registerReceiver(downloadReceiver, filter);
+
+
 
 
 
@@ -74,11 +95,28 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+    }
+
+    private void verifyPermissions() {
+
+        int permissionExt = ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int permissionInternet = ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.INTERNET);
+        ;
+
+
+        if (permissionExt != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, PERMISSIONS_STORAGE, 1);
+        }
+        if (permissionExt != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, PERMISSIONS_INTERNET, 1);
+        }
+
     }
 
     private long DownloadData(Uri uri) {
 
-        long downloadReference;
+        final long downloadReference;
 
         downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
         DownloadManager.Request request = new DownloadManager.Request(uri);
@@ -87,23 +125,62 @@ public class MainActivity extends AppCompatActivity {
         request.setTitle(titles1.get(0));
 
         //Setting description of request
-        request.setDescription("Android Data download using DownloadManager.");
+        request.setDescription("");
 
         //Set the local destination for the downloaded file to a path within the application's external files directory
-        request.setDestinationInExternalPublicDir((Environment.DIRECTORY_MUSIC), titles1.get(0)+".mp3");
+        request.setDestinationInExternalPublicDir((Environment.DIRECTORY_MUSIC), titles1.get(0) + ".mp3");
 
 
         //Enqueue download and save the referenceId
         downloadReference = downloadManager.enqueue(request);
 
+        final ProgressBar progressBar = (ProgressBar) findViewById(R.id.probar);
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                boolean downloading = true;
+                while (downloading) {
+                    DownloadManager.Query q = new DownloadManager.Query();
+                    q.setFilterById(downloadReference);
+                    Cursor c = downloadManager.query(q);
+                    c.moveToFirst();
+
+                    int bytes_downloaded = c.getInt(c.getColumnIndex(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR));
+                    int bytes_total = c.getInt(c.getColumnIndex(DownloadManager.COLUMN_TOTAL_SIZE_BYTES));
+
+                    if (c.getInt(c.getColumnIndex(DownloadManager.COLUMN_STATUS)) == DownloadManager.STATUS_SUCCESSFUL) {
+                        downloading = false;
+                    }
+
+                    final int dl_progress = (int) ((bytes_downloaded * 100l) / bytes_total);
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            progressBar.setProgress((int) dl_progress);
+
+                        }
+                    });
+
+                }
+            }
+        }).start();
 
         return downloadReference;
     }
 
+
+
+
+
+
     private BroadcastReceiver downloadReceiver = new BroadcastReceiver() {
 
         @Override
-        public void onReceive(Context context, Intent intent) { 
+        public void onReceive(Context context, Intent intent) {
 
             //check if the broadcast message is for our Enqueued download
             long referenceId = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1);
@@ -118,6 +195,8 @@ public class MainActivity extends AppCompatActivity {
 
         }
     };
+
+
 
     private void makeJsonObjectRequest(String urlJsonObj) {
 
@@ -158,11 +237,13 @@ public class MainActivity extends AppCompatActivity {
                             }
                         }
                         Log.e("URL For Audio", links.get(0)); {
-                            // TextView testtext = (TextView) findViewById(R.id.testtext);
-                            //testtext.setText("CONVERTED NICE");
 
                             Uri music_uri = Uri.parse(links.get(0));
                             Music_DownloadId = DownloadData(music_uri);
+
+
+
+
 
                         }
                     } catch (JSONException e) {
@@ -186,4 +267,5 @@ public class MainActivity extends AppCompatActivity {
         });
         AppController.getInstance().addToRequestQueue(jsonObjReq);
     }
+
 }
